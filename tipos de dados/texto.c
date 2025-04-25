7e3d2f1c1e7fc9e31ba213ba01fff1136629c38d
isaacbuck <isaacbuck@outlook.com>
2025-04-25 12:23:48 -0300

    salvando

---
+++ tipos de dados/texto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (int argc, char* argv[]){
+char palavra1[] = "conhecimento";
+char palavra2[] = {'C', 'o', 'n', 'h', 'e', 'c', 'i', 'm', 'e', 'n', 't', 'o'};
+
+printf("%s/n", palavra1);
+printf("%s/n", palavra2);
+  
+
+
+
+
+
+}
